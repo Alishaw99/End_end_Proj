a60ebbaf87a371b3b476e72fec26aedb922acfc3
Alishaw99 <tariqham@gmail.com>
2024-02-05 18:00:38 -0800

    data_transformation_added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -9,13 +9,19 @@ class DataIngestionConfig:
     local_data_file: Path
     unzip_dir: Path
     
-from dataclasses import dataclass
-from pathlib import Path
-
-
 @dataclass(frozen=True)
 class DataValidationConfig:
     root_dir: Path
     STATUS_FILE: str
     unzip_data_dir: Path
     all_schema: dict
+    
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
